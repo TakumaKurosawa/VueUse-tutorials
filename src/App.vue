57a60b40423c704816ce15114d079070d90f4d11
Takumaron <tennis.takumax@gmail.com>
2022-11-19 17:46:15 +0900

    add Logo component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <header>
+    <Logo height="50px" link-to="/" />
     <nav>
       <router-link to="/">Home</router-link>
       <router-link to="/about">About</router-link>
@@ -13,15 +14,21 @@
 
 <script lang="ts" setup>
 import { RouterView } from "vue-router";
+import Logo from "@/components/Logo.vue";
 </script>
 
 <style scoped>
 header {
+  height: 70px;
+  background-color: #f5f5f5;
+  display: flex;
+  justify-content: space-between;
+}
+
+nav {
   display: flex;
   justify-content: flex-start;
   align-items: center;
-  height: 70px;
-  background-color: #f5f5f5;
   padding: 1rem;
 }
 
---
+++ src/components/Logo.vue
@@ -0,0 +1,22 @@
+<template>
+  <router-link :to="linkTo" class="logo">
+    <img :style="{ width, height }" alt="logo" src="@/assets/vueuse-logo.png" />
+    <p>VueUse tutorials</p>
+  </router-link>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  width?: string;
+  height?: string;
+  linkTo: string;
+}>();
+</script>
+
+<style scoped>
+.logo {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
