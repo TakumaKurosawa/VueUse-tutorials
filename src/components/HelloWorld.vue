9c48c821f0bcd5bb9467305583e81d880013cf62
Takumaron <tennis.takumax@gmail.com>
2022-11-19 17:00:36 +0900

    add vueuse library

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,21 @@
-<script setup lang="ts">
-defineProps<{
-  msg: string
-}>()
-</script>
-
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a target="_blank" href="https://vitejs.dev/">Vite</a> +
-      <a target="_blank" href="https://vuejs.org/">Vue 3</a>. What's next?
-    </h3>
-  </div>
+  <h3>Mouse: {{ x }} x {{ y }}</h3>
+  <h3>
+    Counter: {{ count }}
+    <a style="margin-right: 10px" @click="inc()">+</a>
+    <a @click="dec()">-</a>
+  </h3>
 </template>
 
-<style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  top: -10px;
-}
+<script lang="ts" setup>
+import { useCounter, useMouse } from "@vueuse/core";
 
+const { x, y } = useMouse();
+const { count, inc, dec } = useCounter();
+</script>
+
+<style scoped>
 h3 {
   font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
 }
 </style>
